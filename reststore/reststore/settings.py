b1e8a51db77a2b7935e32ac24c443e5569e50963
papismurf <33459750+papismurf@users.noreply.github.com>
2018-03-30 02:01:31 +0100

    created postgres database and added to global settings (settings.py)

--- reststore/reststore/settings.py
+++ reststore/reststore/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'reststore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rest_store',
+        'USER': 'postgres',
+        'PASSWORD': 'lavina30',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
