c1ed2a40428af4492d4bd866a2907338a053f29a
papismurf <33459750+papismurf@users.noreply.github.com>
2018-03-30 12:30:07 +0100

    updated product model

--- reststore/products/models.py
+++ reststore/products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Product(models.Model):
     """
     Product model
@@ -13,3 +14,9 @@ class Product(models.Model):
     brand = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
+    def get_brand(self):
+        return self.name + ' product number %s' + ' is ' + self.brand % self.idnum
+
+    def __repr__(self):
+        return self.name + ' product number %s' + ' is added.'
