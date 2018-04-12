d19672b147e2d87ba8e367b71e05480d0439755a
papismurf <33459750+papismurf@users.noreply.github.com>
2018-04-12 10:45:41 +0100

    created view function

--- reststore/products/views.py
+++ reststore/products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+# Create a view function that maps them with their appropriate urls
+@api_view(['GET', 'DELETE', 'PUT'])
+def get_delete_update_product(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
